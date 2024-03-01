56387f9cb2ea778b6d097ed4a15f092b1b7e4def
Heverton Luan Roieski <hevertonlr@gmail.com>
2024-03-01 19:44:41 -0300

    fist commit

---
+++ src/test/java/br/com/fullstack/educational/EducationalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.educational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
